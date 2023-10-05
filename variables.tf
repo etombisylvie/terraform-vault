ef77cb170a1e97de1b6a7a65bf8f97df37702a5f
Njoki88 <tabbz08@yahoo.com>
2023-10-05 16:08:28 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  default = "AKIA5VNCSL6VPU3GXMQS"
+
+}
+variable "secret_key" {
+  default = "+lNCUhQ9LRF0V1lT080LmvDl8DpkwT0y4dGpoHbi"
+
+}
+variable "vault_addr" {
+  default = "http://127.0.0.1:8200"
+
+}
+variable "vault_token" {
+  default = "hvs.m9874m46oP2WqfpsUn9hc64s"
+
+}
